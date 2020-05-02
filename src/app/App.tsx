bcdcc9095d2d3663f2ed2ffd57e1cb7b3a7cf9bb
Fabio Bedini <io@ilwebdifabio.it>
2020-05-02 10:31:33 +0200

    missing dependency: dispatch

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -12,7 +12,7 @@ function App() {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(loadTodos());
-  }, []);
+  }, [dispatch]);
   const addTodo = (data: Todo) => {
     dispatch(addTodos(data));
   };
